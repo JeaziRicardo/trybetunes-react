bd283ac219d7e60c5eb5d4a922890793aff3370f
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-18 17:32:34 -0300

    Cria as tags do cabeçalho no componente App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <header>
+          <h1>TrybeTunes</h1>
+        </header>
+      </div>
+    );
   }
 }
 
