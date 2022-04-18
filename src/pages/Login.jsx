0312419418c9b1af6be3999a07fd554ca11a7a93
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-18 20:37:48 -0300

    Cria o formulário de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login"> </div>
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="loginName">
+            <input type="text" name="loginName" placeholder="NOME" />
+          </label>
+
+          <button type="button" disabled="">Entrar</button>
+
+        </form>
+      </div>
     );
   }
 }
