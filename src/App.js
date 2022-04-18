cdca5553e2c81fe6600af27de33acb5090208d3d
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-18 17:40:28 -0300

    Cria a rota para Login no App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 class App extends React.Component {
   render() {
@@ -7,6 +9,9 @@ class App extends React.Component {
         <header>
           <h1>TrybeTunes</h1>
         </header>
+        <Switch>
+          <Route path="/" component={ Login } />
+        </Switch>
       </div>
     );
   }
