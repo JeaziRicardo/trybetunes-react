5bd51e46d5498002e1d221922c3468d3b6b2ed62
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-18 20:43:46 -0300

    Adiciona os atributos de teste no input e botão

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,10 +6,21 @@ class Login extends Component {
       <div data-testid="page-login">
         <form>
           <label htmlFor="loginName">
-            <input type="text" name="loginName" placeholder="NOME" />
+            <input
+              type="text"
+              name="loginName"
+              placeholder="NOME"
+              data-testid="login-name-input"
+            />
           </label>
 
-          <button type="button" disabled="">Entrar</button>
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled=""
+          >
+            Entrar
+          </button>
 
         </form>
       </div>
