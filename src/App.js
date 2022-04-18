9e355e4cbbaf67cac2ee753c7ccc23b71acfb62a
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-18 17:53:19 -0300

    Cria a rota para Search no App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
         </header>
         <Switch>
           <Route path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
         </Switch>
       </div>
     );
