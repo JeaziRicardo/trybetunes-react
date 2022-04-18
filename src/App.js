f45ef657e0712a7f961d0c06f735537306890edc
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-18 18:49:47 -0300

    Cria a rota para Favorites no App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Album from './pages/Album';
+import Favorites from './pages/Favorites';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
@@ -18,6 +19,7 @@ class App extends React.Component {
           <Route path="/" component={ Login } />
           <Route path="/search" component={ Search } />
           <Route path="/album/:id" component={ Album } />
+          <Route path="favorites" component={ Favorites } />
           <Route path="/profile" component={ Profile } />
           <Route path="/profile/edit" component={ ProfileEdit } />
           <Route path="*" component={ NotFound } />
