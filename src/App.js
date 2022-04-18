8500947768cd0119dff6b6ee8d120deeeef08806
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-18 18:27:25 -0300

    Cria a rota para ProfileEdit no App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Album from './pages/Album';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
 
 class App extends React.Component {
@@ -17,6 +18,7 @@ class App extends React.Component {
           <Route path="/search" component={ Search } />
           <Route path="/album/:id" component={ Album } />
           <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
         </Switch>
       </div>
     );
