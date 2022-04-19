d212aaea58aafbca880f00e9f444443aac166887
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-19 20:03:02 -0300

    Cria a função que controla o valor do input

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      artistName: '',
+    };
+  }
+
+  hadleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      artistName: value,
+    });
+  }
+
   render() {
+    const { artistName } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form action="">
-          <label htmlFor="nome-do-artista">
+        <form>
+          <label htmlFor={ artistName }>
             <input
               type="text"
-              name="nome-do-artista"
+              name={ artistName }
               placeholder="Nome do Artista"
               data-testid="search-artist-input"
+              onChange={ this.hadleChange }
             />
           </label>
 
