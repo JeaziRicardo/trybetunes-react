55d16165bc0ad9660600d99b36f07dbed9367608
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-19 18:31:35 -0300

    Chama o componentDidMount para recuperar o nome da pessoa logada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loginName: '',
+    };
+  }
+
+  componentDidMount = async () => {
+    const { name } = await getUser();
+    this.setState({
+      loginName: name,
+    });
+  }
+
   render() {
     return (
       <header data-testid="header-component">
