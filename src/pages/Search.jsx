f3d81a7c4e7d34f3fb562c1ed9f9294695f42334
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-19 19:52:34 -0300

    Cria o formulário dentro do componente Search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -6,6 +6,23 @@ class Search extends Component {
     return (
       <div data-testid="page-search">
         <Header />
+        <form action="">
+          <label htmlFor="nome-do-artista">
+            <input
+              type="text"
+              name="nome-do-artista"
+              placeholder="Nome do Artista"
+              data-testid="search-artist-input"
+            />
+          </label>
+
+          <button
+            type="button"
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
