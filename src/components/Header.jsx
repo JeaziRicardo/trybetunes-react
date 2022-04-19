948bed2c40b92725f408d5cec40b1f3ea7d1c90d
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-19 18:35:38 -0300

    Exibe o nome do usuário no header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends Component {
   constructor() {
     super();
     this.state = {
       loginName: '',
+      loading: true,
     };
   }
 
@@ -13,13 +15,16 @@ class Header extends Component {
     const { name } = await getUser();
     this.setState({
       loginName: name,
+      loading: true,
     });
   }
 
   render() {
+    const { loginName, loading } = this.state;
     return (
       <header data-testid="header-component">
         <h1>TrybeTunes</h1>
+        { loading ? <Loading /> : <p data-testid="header-user-name">{ loginName }</p> }
       </header>
     );
   }
