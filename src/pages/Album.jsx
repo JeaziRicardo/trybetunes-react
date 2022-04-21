ac17686b0441f77c6ce15d93864903045e853619
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-20 23:36:04 -0300

    Chama a função getMusics ao entrar na página Album

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      musics: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    const { match: { params: { id } } } = this.props;
+    const musics = await getMusics(id);
+    this.setState({
+      musics,
+    });
+  }
+
   render() {
     return (
       <div data-testid="page-album">
@@ -10,5 +27,13 @@ class Album extends Component {
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default Album;
