5a6d77f3fdde52319a1c515ce8b9ca0ddd50e77f
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-22 12:03:27 -0300

    Cria o component MusicCard para exibir as músicas do album

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends Component {
+  render() {
+    const { musics, artworkUrl100, collectionName, artistName } = this.props;
+    return (
+      <section>
+        <section>
+          <img src={ artworkUrl100 } alt={ collectionName } />
+          <h3 data-testid="album-name">{ collectionName }</h3>
+          <p data-testid="artist-name">{ artistName }</p>
+        </section>
+        <section>
+          {musics.map(({ trackId, trackName, previewUrl }, index) => index > 0 && (
+            <div key={ trackId }>
+              <h3>{ trackName }</h3>
+              <audio data-testid="audio-component" src={ previewUrl } controls>
+                <track kind="captions" />
+                O seu navegador não suporta o elemento
+                {' '}
+                <code>audio</code>
+                .
+              </audio>
+            </div>
+          ))}
+        </section>
+      </section>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  musics: PropTypes.arrayOf(PropTypes.object).isRequired,
+  artworkUrl100: PropTypes.string.isRequired,
+  collectionName: PropTypes.string.isRequired,
+  artistName: PropTypes.string.isRequired,
+};
+
+export default MusicCard;
