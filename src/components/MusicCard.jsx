1a6f6e851ce0ec5605214b74779fc42848a4e86e
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-22 23:40:28 -0300

    Cria o mecanismo  para salvar as músicas favoritas

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,36 +1,64 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      checked: '',
+    };
+  }
+
+  handleChange = async ({ target }) => {
+    const { song } = this.props;
+    const { checked } = target;
+    this.setState({ loading: true });
+    if (checked) {
+      await addSong(song);
+    }
+    this.setState({ checked, loading: false });
+  };
+
   render() {
     const { trackId, previewUrl, trackName } = this.props;
+    const { loading, checked } = this.state;
     return (
       <section>
-        <h3>{ trackName }</h3>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          {' '}
-          <code>audio</code>
-          .
-        </audio>
-        <label htmlFor="favorite">
-          Favorita
-          <input
-            data-testid={ `checkbox-music-${trackId}` }
-            type="checkbox"
-            name="favorite"
-          />
-        </label>
+        {loading ? <Loading /> : (
+          <section>
+            <h3>{ trackName }</h3>
+            <audio data-testid="audio-component" src={ previewUrl } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              {' '}
+              <code>audio</code>
+              .
+            </audio>
+            <label htmlFor="favorite">
+              Favorita
+              <input
+                data-testid={ `checkbox-music-${trackId}` }
+                type="checkbox"
+                name="favorite"
+                checked={ checked }
+                onChange={ this.handleChange }
+              />
+            </label>
+          </section>
+        )}
       </section>
     );
   }
 }
 
 MusicCard.propTypes = {
-  trackId: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
   previewUrl: PropTypes.string.isRequired,
   trackName: PropTypes.string.isRequired,
+  song: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
 export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
 class Album extends Component {
   constructor() {
@@ -34,6 +35,15 @@ class Album extends Component {
               <h3 data-testid="album-name">{ musics[0].collectionName }</h3>
               <p data-testid="artist-name">{ musics[0].artistName }</p>
             </section>
+            {musics.map((music, index) => index > 0 && (
+              <MusicCard
+                key={ index }
+                trackId={ music.trackId }
+                previewUrl={ music.previewUrl }
+                trackName={ music.trackName }
+                song={ music }
+              />
+            ))}
           </section>
         )}
       </main>
